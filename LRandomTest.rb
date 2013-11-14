4d2c0d07eb35a0b818a1e8a6e63359ec081dc8e8
Patches Barringer <pbarring@mines.edu>
2013-11-13 18:33:35 -0700

    Added a basic test for shuffle

--- LRandomTest.rb
+++ LRandomTest.rb
@@ -38,6 +38,7 @@ class TestLRandom < Test::Unit::TestCase
     assert_equal([3,9,27,19,26,16,17,20,29,25],res,"Bad stream 0 generation")
     assert_equal([30,28,22,4,12,5,15,14,11,2],res2,"Bad stream 1 generation")
   end
+=begin
   def test_with_authers
     a = LRandom.new(1,256)
     256.times do |i|
@@ -45,5 +46,16 @@ class TestLRandom < Test::Unit::TestCase
         assert_equal( a.nextInt(i),line.to_i)
       end
     end
+  end
+=end
+  def test_shuffle
+    a=LRandom.new(1)
+    l=[]
+    10.times do |i|
+      l<< i
+    end
+    a.shuffle(l,0)
+    puts l
+    assert_not_equal(l,[0,1,2,3,4,5,6,7,8,9])
   end
 end
